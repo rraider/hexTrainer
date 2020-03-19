4fbe8fb856e4226b88933b55d53cc5169e0d88bb
rraider <hellmich.leon@gmail.com>
2020-03-19 15:13:16 +0100

    cleanup ....again

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'home'
-
-urlpatterns = [
-	path('train/', views.start_view, name='train'),
-	path('scoreboard/', views.scoreboard, name='scoreboard')
-]
--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-	xp = models.IntegerField(default=0)
-
-	def __str__(self):
-		return self.username
